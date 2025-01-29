c8baa1cf2d40a97401e71a2129cf98d3c4da1c6a
yohaisim <yohaisimhony@gmail.com>
2025-01-29 15:17:14 +0200

    Updated Flask app with Vercel handler

--- FlaskAdsServer1/FlaskAdsServer1.py
+++ FlaskAdsServer1/FlaskAdsServer1.py
@@ -118,3 +118,6 @@ def update_click_count():
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
 
+    # Handler for Vercel
+def handler(event, context):
+    return app
