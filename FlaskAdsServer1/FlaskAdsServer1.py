e7902c6b4f6cd419633d59f8b5440a7ec32376cc
yohaisim <yohaisimhony@gmail.com>
2025-01-29 15:24:12 +0200

    Fixed Vercel deployment

--- FlaskAdsServer1/FlaskAdsServer1.py
+++ FlaskAdsServer1/FlaskAdsServer1.py
@@ -115,9 +115,11 @@ def update_click_count():
     return jsonify({"message": "Click count updated"}), 200
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
 
-    # Handler for Vercel
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
+
+# Handler for Vercel
 def handler(event, context):
     return app
