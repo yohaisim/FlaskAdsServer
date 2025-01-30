2ab2eb8121ebfa41e745037b4e10cb902ae896f5
yohaisim <yohaisimhony@gmail.com>
2025-01-30 11:09:11 +0200

    runtime added

--- FlaskAdsServer1/server.py
+++ FlaskAdsServer1/server.py
@@ -116,10 +116,8 @@ def update_click_count():
 
 
 
-# ----------------------  LOCAL RUN ----------------------
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
 
-# ---------------------- HANDLER FOR VERCEL (WSGI SIGNATURE) ----------------------
-def handler(environ, start_response):
-    return app.wsgi_app(environ, start_response)
+def handler(event, context):
+    return app(event, context)
